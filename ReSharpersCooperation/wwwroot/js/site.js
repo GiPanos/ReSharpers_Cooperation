ebcdfd2b36e736eba12daf4c872a4705f64eeb66
[Sofoklis] <33422958+politis-sofoklis@users.noreply.github.com>
2018-01-11 16:33:28 +0200

    Navbar + Search

--- ReSharpersCooperation/wwwroot/js/site.js
+++ ReSharpersCooperation/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 ﻿// Write your JavaScript code.
+
+//SEARCH JS BEGIN
+$(document).ready(function (e) {
+    $('.search-panel .dropdown-menu').find('a').click(function (e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#", "");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
+});
+//SEARCH JS END
