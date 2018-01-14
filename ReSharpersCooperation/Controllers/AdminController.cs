42d29ab51840392adcf815754d6651e4b79f62a9
politis.sofoklis <politis.sofoklis@gmail.com>
2018-01-15 00:01:38 +0200

    Created Admin Controller and Create/Edit/Deletee functions for product

---
+++ ReSharpersCooperation/Controllers/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReSharpersCooperation.Models;
+
+namespace ReSharpersCooperation.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private ProductRepository _repository;
+        public AdminController(ProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index()
+        {
+            //var result = User.IsInRole("SuperAdmin");
+            return View(_repository.Products);
+        }
+
+        public ViewResult Edit(int productNo)
+        {
+            var temp = _repository.Products.FirstOrDefault(p => p.ProductNo == productNo);
+            return View(temp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.UpdateProduct(product);
+                TempData["Message"] = $"{product.ProductName} has been updated.";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            var lastproduct = _repository.Products.Last();
+            
+            return View(new Product{ ProductNo = lastproduct.ProductNo + 1 });
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.SaveProduct(product);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(product);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            var product = _repository.DeleteProduct(productId);
+
+            if (product != null)
+            {
+                TempData["Message"] = $"{product.ProductName} was deleted.";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
